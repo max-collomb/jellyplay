0027189bfd09dafbc9745722640b21f118783180
max-collomb <maxime.collomb@gmail.com>
2022-08-26 22:16:07 +0200

    ajout HOME dans commandes start/stop pour faire plais' à DSM

--- api/server-start.sh
+++ api/server-start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /volume1/server/jellyplay/api
-./node_modules/pm2/bin/pm2 start dist/index.js --node-args="--experimental-specifier-resolution=node" --time
+PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 startdist/index.js --node-args="--experimental-specifier-resolution=node" --time
--- api/server-stop.sh
+++ api/server-stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /volume1/server/jellyplay/api
-./node_modules/pm2/bin/pm2 stop dist/index.js
+PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 stop dist/index.js
