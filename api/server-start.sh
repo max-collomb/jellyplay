fed067b46bbd35d444113108227a3b52f08d3f26
max-collomb <maxime.collomb@gmail.com>
2022-08-28 14:41:56 +0200

    ajout d'une sauvegarde de la bdd à chaque démarrage conservation pendant 30j

--- api/server-start.sh
+++ api/server-start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd /volume1/server/jellyplay/api
+cp db/_volume1_share_media_movies db/backups/_volume1_share_media_movies.$(date +%Y-%m-%d_%Hh%Mm%Ss)
+find ./db/backups -name "_volume1_share_media_movies.*" -type f -mtime +30 -delete
 PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 start dist/index.js --node-args="--experimental-specifier-resolution=node" --time
