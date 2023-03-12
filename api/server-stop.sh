ffac8ca712817fc7f72f0390e26bcc955fcb2f91
max-collomb <maxime.collomb@gmail.com>
2023-03-12 18:11:19 +0100

    refactoring server-stop.sh avec nodemon

--- api/server-stop.sh
+++ api/server-stop.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-# cd /volume1/server/jellyplay/api
+cd /volume1/server/jellyplay/api
 # PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 stop dist/index.js
+npx kill-port 3000
