4612ed21714db51a2fdb114d8d28883ed57e548e
max-collomb <maxime.collomb@gmail.com>
2023-03-12 18:31:53 +0100

    annulation pm2 => nodemon

--- api/server-start.sh
+++ api/server-start.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 cd /volume1/server/jellyplay/frontend
 npm run build
 cd /volume1/server/jellyplay/api
 cp db/_volume1_share_media_movies db/backups/_volume1_share_media_movies.$(date +%Y-%m-%d_%Hh%Mm%Ss)
 find ./db/backups -name "_volume1_share_media_movies.*" -type f -mtime +30 -delete
-# npm run build
-# PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 start dist/index.js --node-args="--experimental-specifier-resolution=node" --time
-npx nodemon --experimental-specifier-resolution=node dist/index.js
+npm run build
+PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 start dist/index.js --node-args="--experimental-specifier-resolution=node" --time
--- api/server-stop.sh
+++ api/server-stop.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 cd /volume1/server/jellyplay/api
-# PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 stop dist/index.js
-npx kill-port 3000
+PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 stop dist/index.js
