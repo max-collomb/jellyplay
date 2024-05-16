253815b33ccc599a598ae0acb9ca09fdf8ddc2bd
max collomb <maxime.collomb@gmail.com>
2024-05-16 22:28:06 +0200

    compatibilité node 20

--- api/server-start.sh
+++ api/server-start.sh
@@ -5,4 +5,4 @@ cd /volume1/server/jellyplay/api
 cp db/_volume1_share_media_movies db/backups/_volume1_share_media_movies.$(date +%Y-%m-%d_%Hh%Mm%Ss)
 find ./db/backups -name "_volume1_share_media_movies.*" -type f -mtime +30 -delete
 npm run build
-PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 start dist/index.js --node-args="--experimental-specifier-resolution=node" --time
+PM2_HOME='/var/services/homes/administrator/.pm2' ./node_modules/pm2/bin/pm2 start dist/index.js --node-args="--import=extensionless/register" --time
